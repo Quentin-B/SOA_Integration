24261d8adcacc601e8547d441f130e6ef4505625
camillie <camillie@polytech.unice.fr>
2014-11-18 18:16:06 +0100

    post processing improvment

--- integration/src/main/java/fr/unice/polytech/groupe/integration/exceptions/UnknownResourceException.java
+++ integration/src/main/java/fr/unice/polytech/groupe/integration/exceptions/UnknownResourceException.java
@@ -7,6 +7,10 @@ public class UnknownResourceException extends Exception {
 
 	private static final long serialVersionUID = 1L;
 
+	public UnknownResourceException(String type) {
+		super("Unknown " + type);
+	}
+	
 	public UnknownResourceException(String type, String id) {
 		super("Unknown " + type + " : '" + id + "'");
 	}
